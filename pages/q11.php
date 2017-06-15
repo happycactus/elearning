4b86122ec1bc66d15a0bc83f159eef4229532c18
Lars van de Donk <l.vandedonk@gmail.com>
2017-06-15 11:12:42 +0200

    Undo files split

--- pages/q11.php
+++
@@ -1,7 +0,0 @@
-<div class="moveable" id="q11">
-	<h2>Welke handvatten heb je tot je beschikking?</h2>
-	<div class="blockcenter">
-		<!-- //TODO: COMPLETED RING -->
-		<img src="images/ringcomplete.png" alt="ring" width="100">
-	</div>
-</div>
--- pages/q31.php
+++
@@ -1,9 +0,0 @@
-                        <div class="moveable" id="q31">
-                            <h2>Opzet STeP studie</h2>
-                            <p>Doel: De effectiviteit van gestructureerde bloedglucosetesten bij slecht gecontroleerde, niet-insuline behandelde mensen met diabetes mellitus type 2.</p>
-                            <div class="inner-wrap">
-                                <div class="blockcenter">
-                                    <img src="images/path.png" style="max-width:600px" width="100%">
-                                </div>
-                            </div>
-                        </div>
